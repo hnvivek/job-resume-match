0f746515108a5baec62940053b5f66281506328b
Vivek Reddy <hnvivek@gmail.com>
2024-09-16 00:11:02 -0500

    prompt update

--- llm/generate_cover_letter.py
+++ llm/generate_cover_letter.py
@@ -16,7 +16,7 @@ def generate_cover(model, job_description, resume_content, skills_keywords,
         ("system", f"{sys_prompt}"),
         ("human",
         f"""The Current date is {today} Please generate a unique cover letter 
-        with in 500 words tailored for the Job description : [{job_description}] using 
+        with in 400 words tailored for the Job description : [{job_description}] using 
         my resume: [{resume_content}].
         I'm very interested in this position and want to grab the hiring manager's attention.
         Include my name, relevant skills, work history, and past employers and projects from my resume.
